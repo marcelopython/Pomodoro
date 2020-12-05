b9a01fbca34ee1cab8769068ad63331c55070985
marcelopython <msr88957@gmail.com>
2020-12-05 19:20:03 -0300

    start work

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import {useInterval} from '../hooks/use-interval'
-import {Button} from './button';
-import {Timer} from './timer';
+import React, { useEffect } from 'react';
+import { useInterval } from '../hooks/use-interval'
+import { Button } from './button';
+import { Timer } from './timer';
 
 interface Props {
     pomodoroTime: number;
@@ -10,19 +10,34 @@ interface Props {
     cycles: number
 }
 
-export function PomodoroTimer(props: Props): JSX.Element{
-    const [mainTime, setMainTime] = React.useState(props.pomodoroTime)
-    useInterval(()=>{
+export function PomodoroTimer(props: Props): JSX.Element {
+    const [mainTime, setMainTime] = React.useState(props.pomodoroTime);
+    const [timeCounting, setTimeCounting] = React.useState(false);
+    const [working, setWorking] = React.useState(false);
+
+    useEffect(()=>{
+        if(working){
+            document.body.classList.add('working')
+        }
+    }, [working]);
+
+    useInterval(() => {
         setMainTime(mainTime - 1);
-    }, 1000);
+    }, timeCounting ? 1000 : null);
+
+    const configurerWork = () => {
+        setTimeCounting(true);
+        setWorking(true);
+    }
+
     return (
         <div className="pomodoro">
             <h2>You are: working</h2>
-            <Timer mainTime={mainTime}/>
+            <Timer mainTime={mainTime} />
             <div className="controls">
-                <Button text="teste" onclick={()=>console.log(1)}/>
-                <Button text="teste" onclick={()=>console.log(1)}/>
-                <Button text="teste" onclick={()=>console.log(1)}/>
+                <Button text="Work" onclick={() => configurerWork()} />
+                <Button text="teste" onclick={() => console.log(1)} />
+                <Button text={timeCounting ? 'Pause' : 'Play'} onclick={() => setTimeCounting(!timeCounting)} />
             </div>
             <div className="details">
                 <p>Testando: </p>
