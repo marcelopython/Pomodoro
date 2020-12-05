c1aa28ec4c55856e33c8e585a54a94452ad27f52
marcelopython <msr88957@gmail.com>
2020-12-05 19:07:13 -0300

    added buttons

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -4,22 +4,29 @@ import {Button} from './button';
 import {Timer} from './timer';
 
 interface Props {
-    defaultPomodoroTime: number
+    pomodoroTime: number;
+    shortRestTime: number;
+    longRestTime: number;
+    cycles: number
 }
 
 export function PomodoroTimer(props: Props): JSX.Element{
-
-    const [mainTime, setMainTime] = React.useState(props.defaultPomodoroTime)
-
+    const [mainTime, setMainTime] = React.useState(props.pomodoroTime)
     useInterval(()=>{
         setMainTime(mainTime - 1);
     }, 1000);
-
     return (
         <div className="pomodoro">
             <h2>You are: working</h2>
-            <Timer mainTime={360}/>
-            <Button text="teste" onclick={()=>console.log(1)}/>
+            <Timer mainTime={mainTime}/>
+            <div className="controls">
+                <Button text="teste" onclick={()=>console.log(1)}/>
+                <Button text="teste" onclick={()=>console.log(1)}/>
+                <Button text="teste" onclick={()=>console.log(1)}/>
+            </div>
+            <div className="details">
+                <p>Testando: </p>
+            </div>
         </div>
     );
 }
