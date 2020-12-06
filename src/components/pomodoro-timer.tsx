74b3caefe8ebf872358d8903002d526cf8230b49
marcelopython <msr88957@gmail.com>
2020-12-05 22:25:09 -0300

    sounds

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -2,6 +2,10 @@ import React, { useEffect } from 'react';
 import { useInterval } from '../hooks/use-interval'
 import { Button } from './button';
 import { Timer } from './timer';
+const bellStart = require('../sounds/bell-start.mp3');
+const bellFinish = require('../sounds/bell-finish.mp3');
+const audioStartWorking = new Audio(bellStart.default);
+const audioFinishWorking = new Audio(bellFinish.default);
 
 interface Props {
     pomodoroTime: number;
@@ -14,10 +18,14 @@ export function PomodoroTimer(props: Props): JSX.Element {
     const [mainTime, setMainTime] = React.useState(props.pomodoroTime);
     const [timeCounting, setTimeCounting] = React.useState(false);
     const [working, setWorking] = React.useState(false);
+    const [resting, setResting] = React.useState(false);
 
     useEffect(()=>{
         if(working){
             document.body.classList.add('working')
+        }
+        if(resting){
+            document.body.classList.remove('working')
         }
     }, [working]);
 
@@ -28,6 +36,23 @@ export function PomodoroTimer(props: Props): JSX.Element {
     const configurerWork = () => {
         setTimeCounting(true);
         setWorking(true);
+        setResting(false);
+        setMainTime(props.pomodoroTime);
+        audioStartWorking.play();
+    }
+
+    const configurerResting = (long: boolean) => {
+        setTimeCounting(true);
+        setWorking(false);
+        setResting(true);
+        setMainTime(props.pomodoroTime);
+
+        if(long){
+            setMainTime(props.longRestTime);
+        }else{
+            setMainTime(props.shortRestTime);
+        }
+        audioFinishWorking.play();
     }
 
     return (
@@ -36,8 +61,13 @@ export function PomodoroTimer(props: Props): JSX.Element {
             <Timer mainTime={mainTime} />
             <div className="controls">
                 <Button text="Work" onclick={() => configurerWork()} />
-                <Button text="teste" onclick={() => console.log(1)} />
-                <Button text={timeCounting ? 'Pause' : 'Play'} onclick={() => setTimeCounting(!timeCounting)} />
+                <Button text="teste" onclick={() => configurerResting(false)} />
+                <Button text={
+                    timeCounting ? 'Pause' : 'Play'
+                    }
+                    className={!working && !resting ? 'hidden' : ''}
+                    onclick={() => setTimeCounting(!timeCounting)} 
+                />
             </div>
             <div className="details">
                 <p>Testando: </p>
